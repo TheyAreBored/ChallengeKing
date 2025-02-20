64e64ad01821ec35422ed326a8c612ce4a549064
TheyAreBored <theyarebored.com@gmail.com>
2025-02-20 09:21:47 +0000

    Update cloud-parallax.js

--- assets/js/cloud-parallax.js
+++ assets/js/cloud-parallax.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function () {
     let scrollPosition = window.scrollY;
 
     document.querySelector('.cloud_verysmall').style.transform = `translateY(${scrollPosition * 0.6}px)`;
-    ocument.querySelector('.cloud_small').style.transform = `translateY(${scrollPosition * 0.7}px)`;
+    document.querySelector('.cloud_small').style.transform = `translateY(${scrollPosition * 0.7}px)`;
     document.querySelector('.cloud_medium').style.transform = `translateY(${scrollPosition * 0.8}px)`;
     document.querySelector('.cloud_large').style.transform = `translateY(${scrollPosition * 0.9}px)`;
     
